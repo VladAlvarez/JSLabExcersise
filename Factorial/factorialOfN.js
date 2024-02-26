0620fd0231babfbf855fe67d2615d7f56fc47ffb
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2024-02-26 11:24:48 -0700

    Factorial of n using recursion

---
+++ Factorial/factorialOfN.js
@@ -0,0 +1,14 @@
+function factorial(n) {
+    // of course if a number is negative this won't work
+    if (n < 0)
+        return -1;
+    else if (n == 0)
+        return 1;
+    // recursion -- reference itself 
+    else {
+        return (n * factorial(n - 1));
+    }
+}
+
+console.log(factorial(5));
+console.log(factorial(7));
